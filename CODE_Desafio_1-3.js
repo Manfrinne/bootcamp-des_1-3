441ac5d34d7f1f7d09c74b640705747b6778e14a
Manfrinne Ferreira <m4nfrinne@gmail.com>
2020-05-01 14:01:52 -0300

    Check user specific technology

--- CODE_Desafio_1-3.js
+++ CODE_Desafio_1-3.js
@@ -12,6 +12,22 @@ for (let user of users) {
     console.log(`${user.name} trabalha com ${user.technologies.join(', ')}.`)
 }
 
+// Function that checks technology
+function checkTECH (user) {
+    for (let technology of user.technologies) {
+        if (technology == 'CSS') return true 
+    }
+    return false
+} 
+
+// Check if user works with technology
+for (let i = 0; i < users.length; i++) {
+    const userWorkTECH = checkTECH(users[i])
+
+    if (userWorkTECH) {
+        console.log(`O usuário ${users[i].name} trabalha com CSS.`)
+    } 
+}
 
 
 
